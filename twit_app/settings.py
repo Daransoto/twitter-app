62dbcce226988098773f4829f818a430d4c8053f
daransoto <901@holbertonschool.com>
2022-01-08 13:01:00 -0500

    Added djangorestframework in requirements and installed apps

--- twit_app/settings.py
+++ twit_app/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third-party
+    'djangorestframework',
+    # internal
     'tweets',
 ]
 
